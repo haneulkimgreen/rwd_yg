e73ef23d634aef56735ed33db0a7d9d6132ad27c
haneulkimgreen <rlagksmf0918@gmail.com>
2018-03-23 08:57:52 +0900

    delete

--- js/src/yg_01_main.js
+++
@@ -1,9 +0,0 @@
-// yg_01_main.js
-(function($) {
-    // 동영상 아래부터 컨텐츠들 나오게 하기
-    var albumWrap = $('#albumWrap');
-    var hei = $(window).height();
-    // console.log(hei);
-    albumWrap.css({marginTop:hei});
-
-})(this.jQuery);
