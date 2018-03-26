69e6875542eda169754adad6172536616442880d
haneulkimgreen <rlagksmf0918@gmail.com>
2018-03-26 17:19:20 +0900

    180326_p05

--- js/src/yg_05_about.js
+++ js/src/yg_05_about.js
@@ -1,8 +1,8 @@
-// yg_01_main.js
+// yg_05_about.js
 (function($) {
     // 동영상 아래부터 컨텐츠들 나오게 하기
-    var albumWrap = $('#albumWrap');
+    var aboutWrap = $('#aboutWrap');
     var hei = $(window).height();
     // console.log(hei);
-    albumWrap.css({marginTop:hei});
+    aboutWrap.css({marginTop:hei});
 })(this.jQuery);
