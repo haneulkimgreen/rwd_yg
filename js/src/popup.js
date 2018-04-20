1cd68b8a819f4a2391898c752e64d08de3a34861
haneulkimgreen <rlagksmf0918@gmail.com>
2018-04-20 18:50:33 +0900

    artists_link

--- js/src/popup.js
+++ js/src/popup.js
@@ -2,7 +2,7 @@
 (function($) {
   var body = $('body');
   // body내부에 팝업상자 만들기
-  body.prepend('<div id="popupBox"><div class="pop_in"><button tabindex="1" type="button">close</button><p></p></div></div>');
+  body.prepend('<div id="popupBox"><div class="pop_in"><button tabindex="2" type="button">close</button><p></p></div></div>');
 
   var popup = $('#popupBox');
   var popIn = popup.children('.pop_in');
@@ -26,7 +26,7 @@
   });
 
   var narr = popIn.find('p');
-  narr.html('본 사이트는 어떠한 경우라도 상업 및 영리를 목적으로 제작된 페이지가 아니며, <br /> 취업을 목적으로 한 개인 포트폴리오 사이트임을 알려드립니다. <br /> 문제가 되거나, 수정을 원하신다면 메일로 연락주시면 조취를 취하도록 하겠습니다. <br /> rlagksmf0918@gmail.com <br /> 사용된 이미지의 출처는 아래 링크를 통해 확인하세요. <br /> <a tabindex="1" href="./image_link.html" target="_blank">이미지 출처 확인하기<a>');
+  narr.html('본 사이트는 어떠한 경우라도 상업 및 영리를 목적으로 제작된 페이지가 아니며, <br /> 취업을 목적으로 한 개인 포트폴리오 사이트임을 알려드립니다. <br /> 문제가 되거나, 수정을 원하신다면 메일로 연락주시면 조치를 취하도록 하겠습니다. <br /> rlagksmf0918@gmail.com <br /> 사용된 이미지의 출처는 아래 링크를 통해 확인하세요. <br /> <a tabindex="2" href="./image_link.html" target="_blank">이미지 출처 확인하기<a>');
   narr.find('a').css({color:'#d1000a', fontWeight:'bold'});
   narr.find('a').on('focus',function() {
     $(this).css({color:'#fff', outline:0, padding:'0.3rem', borderRadius:'0.2rem'}); 
